e9712418a45a78f13a2959f13a229db255dab14c
Jen Rogers <jen@sci.utah.edu>
2019-09-10 10:48:34 -0600

    fixed the problem with the if statement in draw comparisons

--- src/scripts/selectedPaths.js
+++ src/scripts/selectedPaths.js
@@ -235,6 +235,16 @@ export function renderComparison(group, otherPaths, selectedDiv, scales){
         let newColor = colorKeeper.find(c => usedColors.indexOf(c[0]) === -1);
         group.groupColor = newColor[0];
         comparisonKeeper.push(group);
+
+    }
+
+    if(comparisonKeeper.length > 1){
+        let compareButtonTest = d3.select('#toolbar').select('#compare-button');
+        let compareButton = compareButtonTest.empty() ? d3.select('#toolbar').append('button').text('Normal Mode').attr('id', 'compare-button').classed('btn btn-info', true) : compareButtonTest;
+        compareButton.on('click', ()=> {
+            compareButton.text() === "Normal Mode" ? compareButton.text('Compare Mode') : compareButton.text('Normal Mode');
+            renderComparison(null, otherPaths, selectedDiv, scales);
+        });
     }
 
     let comparisonCombined = scales.map((sc, i)=> {
@@ -305,6 +315,91 @@ export function renderComparison(group, otherPaths, selectedDiv, scales){
     let wrapRect = innerWrap.selectAll('rect.outline-rect').data(d=> [d]).join('rect').classed('outline-rect', true)
                     .attr('width', 800).attr('height', 60).attr('fill', 'none').attr('stroke', 'red');
     
+if(d3.select('#compare-button').empty() || d3.select('#compare-button').text() === "Normal Mode"){
+        let lineGen = d3.line()
+        .x((d, i)=> {
+            let x = d3.scaleLinear().domain([0, 5]).range([0, 800]);
+            return x(i);
+        })
+        .y(d=> {
+            let y = d.yScale;
+            y.range([60, 1]);
+            return y(d.mean);
+        });
+
+    let pathGroups = innerWrap.selectAll('g.path-groups').data(d=> d.data).join('g').classed('path-groups', true);
+    pathGroups.selectAll('*').remove();
+    let paths = pathGroups.append('path').attr('d', d=> { 
+        let scale = d.bins[0].data[0].yScale
+        d.bins = d.bins.map((b, i, n)=> {
+            if(b.mean === undefined){
+                b.mean = d.bins[i-1].mean;
+                d.missing = true;
+            }
+            b.yScale = scale;
+            return b;
+        });
+        return lineGen(d.bins);
+    });
+
+    paths.style('fill', 'none');
+    paths.style('stroke', d=> {
+        return d.group.color;
+    });
+    paths.style('stroke-width', '1px');
+    
+}else{
+
+    innerWrap.selectAll('.path-groups').remove();
+    let pathGroups = innerWrap.selectAll('g.path-groups').data(d=> {
+        let startBins = d.data[0].bins;
+        let difArray = [];
+        for(let i = 1; i < d.data.length; i ++){
+            let diffs = []
+            d.data[i].bins.map((b, j)=>{
+                if(b.mean === undefined){
+                    b.mean = d.data[i].bins[j-1].mean;
+                }
+                if(startBins[j].mean === undefined){
+                    startBins[j].mean = startBins[j-1].mean;
+                }
+                diffs.push(Math.abs(startBins[j].mean - b.mean));
+            });
+            difArray.push(diffs);
+        }
+        return difArray;
+    }).join('g').classed('path-groups', true);
+
+    let lineGen = d3.line()
+        .x((d, i)=> {
+            let x = d3.scaleLinear().domain([0, 5]).range([0, 800]);
+            return x(i);
+        })
+        .y(d=> {
+            let y = d3.scaleLinear().domain([0, 5]).clamp(true);
+            y.range([60, 0]);
+            return y(d);
+        });
+
+    let paths = pathGroups.append('path').attr('d', d=> { 
+        return lineGen(d);
+    });
+
+    paths.style('fill', 'none');
+    paths.style('stroke', 'black');
+    paths.style('stroke-width', '1px');
+
+
+}
+
+
+
+
+
+
+
+/////////////////////////
+/*
     if(comparisonKeeper.length > 1){
         let compareButtonTest = d3.select('#toolbar').select('#compare-button');
         let compareButton = compareButtonTest.empty() ? d3.select('#toolbar').append('button').text('Normal Mode').attr('id', 'compare-button').classed('btn btn-info', true) : compareButtonTest;
@@ -423,7 +518,7 @@ export function renderComparison(group, otherPaths, selectedDiv, scales){
         });
         paths.style('stroke-width', '1px');
 
-    }
+    }*/
 }
 export function renderSelectedView(pathData, otherPaths, selectedDiv, scales, moveMetric) {
 
