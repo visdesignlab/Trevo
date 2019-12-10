7c84775ddc1abf775f96b41b444eff51892fdc09
Jen Rogers <jen@sci.utah.edu>
2019-12-10 15:49:42 -0700

    changed multinet object to a local variable

--- src/scripts/multinetLoad.js
+++ src/scripts/multinetLoad.js
@@ -2,13 +2,15 @@ import { multinetApi } from "multinet";
 
 /* Multinet data importer */
 // Define local variables that will store the api url and the responses from the database
-let multinet = {
-    tables: {},
-    nodes: [],
-    links: [],
-    graph_structure: {},
-    api_root: "https://multinet.app/api"
-};
+// let multinet = {
+//     tables: {},
+//     nodes: [],
+//     links: [],
+//     graph_structure: {},
+//     api_root: "https://multinet.app/api"
+// };
+const api_root = "https://multinet.app/api";
+
 class Multinet {
 
     constructor(){
@@ -17,53 +19,55 @@ class Multinet {
         this.nodes = [],
         this.links = [],
         this.graph_structure = {},
-        this.api_root = "https://multinet.app/api"
+        this.api_root = api_root
 
     }
    
 }
 
-const api = multinetApi(multinet.api_root);
+const api = multinetApi(api_root);
 
 export async function getGraphNames(workspace){
-  
     return await api.graphs(workspace);
 }
 
 export async function load_data(workspace, graph) {
+
+    let multinetOb = new Multinet();
+
     // Fetch the names of all the node and edge tables 
-    await load_tables(workspace, graph);
+    await load_tables(workspace, graph, multinetOb);
 
     // Loop through each node tables and fetch the nodes to global variables
-    for (let node_table of multinet.tables.nodeTables) {
-        await load_nodes(workspace, node_table);
+    for (let node_table of multinetOb.tables.nodeTables) {
+        await load_nodes(workspace, node_table, multinetOb);
     };
 
     // Load the edge table (ONLY ONE BECAUSE OF ARANGO API LIMITATIONS) to a global variable
-    let edge_table = multinet.tables.edgeTable;
-    await load_links(workspace, edge_table);
+    let edge_table = multinetOb.tables.edgeTable;
+    await load_links(workspace, edge_table, multinetOb);
 
     // Set the graph structure
-    multinet.graph_structure = { "nodes": rename_node_vars(multinet.nodes), "links": rename_link_vars(multinet.links) }
-    return JSON.parse(JSON.stringify(multinet.graph_structure))
+    multinetOb.graph_structure = { "nodes": rename_node_vars(multinetOb.nodes), "links": rename_link_vars(multinetOb.links) }
+    return JSON.parse(JSON.stringify(multinetOb.graph_structure))
 };
 
-async function load_tables(workspace, graph) {
-    multinet.tables = await api.graph(workspace, graph);
+async function load_tables(workspace, graph, multinetOb) {
+    multinetOb.tables = await api.graph(workspace, graph);
 };
 
-async function load_nodes(workspace, node_table) {
+async function load_nodes(workspace, node_table, multinetOb) {
     const table = await api.table(workspace, node_table, {
       limit: 1000,
     });
-    multinet.nodes = [].concat(multinet.nodes, table);
+    multinetOb.nodes = [].concat(multinetOb.nodes, table);
 };
 
-async function load_links(workspace, edge_table) {
+async function load_links(workspace, edge_table, multinetOb) {
     const table = await api.table(workspace, edge_table, {
       limit: 1000,
     });
-    multinet.links = [].concat(multinet.links, table)
+    multinetOb.links = [].concat(multinetOb.links, table)
 };
 
 function rename_link_vars(links) {
